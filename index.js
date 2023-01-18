2f1a02674988b50e96b581f6156f18c5b55150f5
Colby Shabel <jcshabel@gmail.com>
2023-01-17 18:21:34 -0600

    used timplate litterals and functions plus a fuction in a function to make this

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(roller='roller-skate'){
+    return `This Saturday, I want to ${roller}!`
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(wrap='*', ){
+    return function(hard='a hard worker'){
+        return `You are ${wrap}${hard}${wrap}!`
+    }
+    
+
+}
+wrapAdjective('||' )('a dedicated programmer')
+
